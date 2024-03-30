64ffa9e7d4a9d00cd28e93778c25dc2f1a14f628
Hiro <81205061+Abdenry@users.noreply.github.com>
2024-03-30 10:22:56 +1000

    Added Images, improved layout for mobile and desktop

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,19 +1,28 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import circuit from "@/public/circuit.png";
 
 const NavBar = () => {
   return (
     <header>
       <div className="bg-opacity-80 backdrop-blur-md w-full h-60px fixed border-b border-[#b1b1b118]">
         <div className="text-white flex flex-row sm:justify-evenly p-4 mx-auto justify-start">
-          <div className=" font-extrabold text-xl">
-            <Link href="/">Brayden Annan</Link>
-          </div>
+          {/* Name Div with Logo */}
+          <Link className="flex font-extrabold text-xl group" href="/">
+            <Image
+              className="mx-2 group-hover:-rotate-6"
+              src={circuit}
+              width={30}
+              height={30}
+              alt="Circuit Icon"
+            />
+            <h1>Brayden Annan</h1>
+          </Link>
+
+          {/* Main Links Div */}
           <div className="hidden sm:block">
             <div className="flex flex-row gap-3">
-              <Link className="hover:border-b" href="/About">
-                About
-              </Link>
               <Link className="hover:border-b" href="/Projects">
                 Projects
               </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,24 @@
-import Image from "next/image";
-import houseIsland from "@/public/housesIsland.jpg";
-
 export default function Home() {
   return (
-    <main className="h-[4200px] pt-[60px] bg-[#212121]">
-      <div className="flex flex-col justify-center items-center">
-        <div>
-          <h1 className="text-white"> Content here! </h1>
+    <main className="pt-[60px] bg-[#212121] min-h-screen">
+      <div className="flex flex-col mx-3 sm:mx-12  md:mx-32 lg:mx-54 xl:mx-96 pt-6 text-white justify-end">
+        {/* Greetting Card */}
+        <div className="bg-[#333333] p-6 px-16 mb-6 rounded-lg min-w-[300px]">
+          <h1 className="text-center line-clamp-3 sm:text-base">
+            {" "}
+            Hello, I'm a mechatronics and computer science student from
+            Brisbane!{" "}
+          </h1>
         </div>
-        <div className="pt-2">
-          <Image
-            className="rounded-lg "
-            src={houseIsland}
-            width={500}
-            height={1000}
-            alt="BackDrop"
-          />
+        {/* More Detailed Greeting */}
+        <div className="flex flex-col justify-start">
+          <div>
+            <h1 className="font-extrabold text-3xl text-nowrap">
+              {" "}
+              Brayden Annan
+            </h1>
+          </div>
+          <div> </div>
         </div>
       </div>
     </main>
