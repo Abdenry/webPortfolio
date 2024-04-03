d28bb6f58750446ebf82250a77b62aaef6edc686
Hiro <81205061+Abdenry@users.noreply.github.com>
2024-04-03 18:38:55 +1000

    Made project cards responsive

--- app/Projects/page.tsx
+++ app/Projects/page.tsx
@@ -1,9 +1,93 @@
+import Image from "next/image";
+import project1 from "@/public/simpleGradient.png";
+
 export default function ProjectsPage() {
   return (
-    <main className="pt-[60px] bg-[#212121] min-h-[4000px]">
+    <main className="py-[60px] bg-[#212121] min-h-screen">
       {/* Main Contatiner */}
       <div className="container mx-auto max-w-[600px] pt-6 px-3">
-        <h1 className="text-white px-6">ProjectsPage</h1>
+        <div className="px-6">
+          {/* Page Description */}
+          <div className="text-white">
+            <h1 className="border-b w-auto font-extrabold sm:text-lg">
+              Projects
+            </h1>
+            <p className=" my-5">
+              {" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Viverra mauris in aliquam sem fringilla ut morbi tincidunt.
+            </p>
+          </div>
+          {/* Project Card 1 */}
+          <div className="flex-col bg-[#444444] min-h-[150px] rounded-lg mb-5 text-white px-2">
+            <h1 className="py-2 text-center">Project 1</h1>
+            <div className="sm:flex sm:pb-2">
+              <Image
+                className=" rounded-md"
+                src={project1}
+                alt="Project 1 Image"
+                layout="responsive"
+              />
+              <p className="p-2 text-center">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Viverra mauris in aliquam sem fringilla ut morbi tincidunt.
+              </p>
+            </div>
+          </div>
+          {/* Project Card 2 */}
+          <div className="flex-col bg-[#444444] min-h-[150px] rounded-lg mb-5 text-white px-2">
+            <h1 className="py-2 text-center">Project 2</h1>
+            <div className="sm:flex sm:pb-2">
+              <Image
+                className=" rounded-md"
+                src={project1}
+                alt="Project 1 Image"
+                layout="responsive"
+              />
+              <p className="p-2 text-center">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Viverra mauris in aliquam sem fringilla ut morbi tincidunt.
+              </p>
+            </div>
+          </div>
+          {/* Project Card 3 */}
+          <div className="flex-col bg-[#444444] min-h-[150px] rounded-lg mb-5 text-white px-2">
+            <h1 className="py-2 text-center">Project 3</h1>
+            <div className="sm:flex sm:pb-2">
+              <Image
+                className=" rounded-md"
+                src={project1}
+                alt="Project 1 Image"
+                layout="responsive"
+              />
+              <p className="p-2 text-center">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Viverra mauris in aliquam sem fringilla ut morbi tincidunt.
+              </p>
+            </div>
+          </div>
+          {/* Project Card 4 */}
+          <div className="flex-col bg-[#444444] min-h-[150px] rounded-lg mb-5 text-white px-2">
+            <h1 className="py-2 text-center">Project 4</h1>
+            <div className="sm:flex sm:pb-2">
+              <Image
+                className=" rounded-md"
+                src={project1}
+                alt="Project 1 Image"
+                layout="responsive"
+              />
+              <p className="p-2 text-center">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Viverra mauris in aliquam sem fringilla ut morbi tincidunt.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
