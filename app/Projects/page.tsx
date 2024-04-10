68093151e4e9fca7e2758ea5ad4af0d51dcf59df
Hiro <81205061+Abdenry@users.noreply.github.com>
2024-04-10 18:11:41 +1000

    Add EGB320 to projects

--- app/Projects/page.tsx
+++ app/Projects/page.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
-import project1 from "@/public/simpleGradient.png";
+import project1 from "@/public/egb320Project.png";
+import project2 from "@/public/simpleGradient.png";
+import project3 from "@/public/simpleGradient.png";
+import project4 from "@/public/simpleGradient.png";
 
 export default function ProjectsPage() {
   return (
@@ -20,73 +23,86 @@ export default function ProjectsPage() {
             </p>
           </div>
           {/* Project Card 1 */}
-          <div className="flex-col bg-[#444444] min-h-[150px] rounded-lg mb-5 text-white px-2">
-            <h1 className="py-2 text-center">Project 1</h1>
-            <div className="sm:flex sm:pb-2">
-              <Image
-                className=" rounded-md"
-                src={project1}
-                alt="Project 1 Image"
-                layout="responsive"
-              />
-              <p className="p-2 text-center">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Viverra mauris in aliquam sem fringilla ut morbi tincidunt.
-              </p>
+          <a
+            href="https://youtu.be/Dr_o5N65tek?t=120"
+            target="_blank"
+            className="group"
+          >
+            <div className="flex-col bg-[#444444] min-h-[150px] rounded-lg mb-5 text-white px-2 group-hover:scale-105">
+              <h1 className="py-2 text-center">Sample Collecting Robot</h1>
+              <div className="sm:flex sm:pb-2 ">
+                <Image
+                  className="rounded-md max-w-[272px] max-h-[153px]"
+                  src={project1}
+                  alt="Project 1 Image"
+                />
+                <p className="p-2 text-balance text-center text-sm sm:text-start">
+                  In 2022, I completed the EGB320: Mechatronics Design 2 unit
+                  and built a prototype mars rover! The mission was to search,
+                  collect and return "moon rocks" scattered around the
+                  environment.
+                </p>
+              </div>
             </div>
-          </div>
+          </a>
           {/* Project Card 2 */}
-          <div className="flex-col bg-[#444444] min-h-[150px] rounded-lg mb-5 text-white px-2">
-            <h1 className="py-2 text-center">Project 2</h1>
-            <div className="sm:flex sm:pb-2">
-              <Image
-                className=" rounded-md"
-                src={project1}
-                alt="Project 1 Image"
-                layout="responsive"
-              />
-              <p className="p-2 text-center">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Viverra mauris in aliquam sem fringilla ut morbi tincidunt.
-              </p>
+          <a href="" className="group">
+            <div className="flex-col bg-[#444444] min-h-[150px] rounded-lg mb-5 text-white px-2 group-hover:scale-105">
+              <h1 className="py-2 text-center">Project 2</h1>
+              <div className="sm:flex sm:pb-2">
+                <Image
+                  className=" rounded-md max-w-[272px] max-h-[153px]"
+                  src={project2}
+                  alt="Project 1 Image"
+                />
+                <p className="p-2 text-center ">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Viverra mauris in aliquam sem fringilla ut morbi tincidunt.
+                </p>
+              </div>
             </div>
-          </div>
+          </a>
           {/* Project Card 3 */}
-          <div className="flex-col bg-[#444444] min-h-[150px] rounded-lg mb-5 text-white px-2">
-            <h1 className="py-2 text-center">Project 3</h1>
-            <div className="sm:flex sm:pb-2">
-              <Image
-                className=" rounded-md"
-                src={project1}
-                alt="Project 1 Image"
-                layout="responsive"
-              />
-              <p className="p-2 text-center">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Viverra mauris in aliquam sem fringilla ut morbi tincidunt.
-              </p>
+          <a href="" className="group">
+            <div className="flex-col bg-[#444444] min-h-[150px] rounded-lg mb-5 text-white px-2 group-hover:scale-105">
+              <h1 className="py-2 text-center">Project 3</h1>
+              <div className="sm:flex sm:pb-2">
+                <Image
+                  className=" rounded-md max-w-[272px] max-h-[153px]"
+                  src={project3}
+                  alt="Project 1 Image"
+                  width={272}
+                  height={153}
+                />
+                <p className="p-2 text-center ">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Viverra mauris in aliquam sem fringilla ut morbi tincidunt.
+                </p>
+              </div>
             </div>
-          </div>
+          </a>
           {/* Project Card 4 */}
-          <div className="flex-col bg-[#444444] min-h-[150px] rounded-lg mb-5 text-white px-2">
-            <h1 className="py-2 text-center">Project 4</h1>
-            <div className="sm:flex sm:pb-2">
-              <Image
-                className=" rounded-md"
-                src={project1}
-                alt="Project 1 Image"
-                layout="responsive"
-              />
-              <p className="p-2 text-center">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Viverra mauris in aliquam sem fringilla ut morbi tincidunt.
-              </p>
+          <a href="" className="group">
+            <div className="flex-col bg-[#444444] min-h-[150px] rounded-lg mb-5 text-white px-2 group-hover:scale-105">
+              <h1 className="py-2 text-center">Project 4</h1>
+              <div className="sm:flex sm:pb-2">
+                <Image
+                  className=" rounded-md max-w-[272px] max-h-[153px]"
+                  src={project4}
+                  alt="Project 1 Image"
+                  width={272}
+                  height={153}
+                />
+                <p className="p-2 text-center">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Viverra mauris in aliquam sem fringilla ut morbi tincidunt.
+                </p>
+              </div>
             </div>
-          </div>
+          </a>
         </div>
       </div>
     </main>
