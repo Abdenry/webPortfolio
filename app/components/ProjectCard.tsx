9476c778a5e4f3f1fae4b47bb298bfcb3dfd10f5
Hiro <81205061+Abdenry@users.noreply.github.com>
2024-07-11 19:57:29 +1000

    Clean up and refactor of project cards
    All the project cards are now a component with easy to read and navigate values. Can pass a link, target value, image to display and a descriptor.

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,29 +1,39 @@
 import Image from "next/image";
-import project4 from "@/public/simpleGradient.png";
 
 interface CardProps {
   title: string;
   link: string;
+  img: string;
+  desc: string;
+  external: boolean;
 }
 
-const ProjectCard: React.FC<CardProps> = ({ title, link }) => {
+const ProjectCard: React.FC<CardProps> = ({
+  title,
+  link,
+  img,
+  desc,
+  external,
+}) => {
+  const targetVal = external ? "_blank" : "";
   return (
-    <a href={link} target="_blank" className="group">
+    <a
+      href={link}
+      target={targetVal}
+      className="group"
+      rel={external ? "noopener noreferrer" : ""}
+    >
       <div className="flex-col bg-[#444444] min-h-[150px] rounded-lg mb-5 text-white px-2 group-hover:scale-105">
         <h1 className="py-2 text-center">{title}</h1>
         <div className="sm:flex sm:pb-2">
           <Image
             className=" rounded-md max-w-[272px] max-h-[153px]"
-            src={project4}
+            src={img} // Image should be 1280 x 720
             alt="Project 1 Image"
             width={272}
             height={153}
           />
-          <p className="p-2 text-center">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra
-            mauris in aliquam sem fringilla ut morbi tincidunt.
-          </p>
+          <p className="p-2 text-left">{desc}</p>
         </div>
       </div>
     </a>
